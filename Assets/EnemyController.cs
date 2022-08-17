d37c079bdeef513b8ccd00211c9aeed1b61c69da
unknown <[erou1227@yahoo.co.jp]>
2022-08-17 09:08:08 +0900

    敵と誘導を最低限実装

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+    //目的地となるオブジェクトのトランスフォーム格納用
+    public Transform target;
+    //エージェントとなるオブジェクトのNavMeshAgent格納用
+    public NavMeshAgent agent;
+    public NavMeshAgent[] agents;
+    // Use this for initialization
+
+    void Start(){
+
+    }
+
+    void Update()
+    {
+        for(int i=0;i<agents.Length;i++){
+            //目的地となる座標を設定する
+            agents[i].destination = target.position;
+        }
+    }
+}
